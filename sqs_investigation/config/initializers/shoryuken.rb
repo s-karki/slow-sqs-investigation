d6082b00f653775981b77874b44436a10ec6fc3e
s-karki <skarki@seek.com.au>
2024-05-07 17:04:55 +1000

    add some jobengine specific config

--- sqs_investigation/config/initializers/shoryuken.rb
+++ sqs_investigation/config/initializers/shoryuken.rb
@@ -1,3 +1,26 @@
 Rails.application.config.to_prepare do
-  
+  sqs_client_options = {
+    region: "us-west-1",
+    retry_mode: "standard",
+    max_attempts: 2,
+    http_open_timeout: 2,
+    http_read_timeout: 3,
+    log_level: :debug
+  }
+
+  Shoryuken.configure_server do |config|
+    config.server_middleware do |chain|
+      chain.remove Shoryuken::Middleware::Server::Timing
+    end
+
+    config.sqs_client_receive_message_opts = {wait_time_seconds: 20}
+
+    Aws::SQS::Client.remove_plugin(Aws::Plugins::UserAgent)
+
+    config.sqs_client = Aws::SQS::Client.new(sqs_client_options.merge(http_read_timeout: 21))
+  end
+
+  Shoryuken.configure_client do |config|
+    config.sqs_client = Aws::SQS::Client.new(sqs_client_options)
+  end
 end
